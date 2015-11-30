56f8435e652867ef36458f2733e990875e2deb62
Thomas Brand <tom@trellis.ch>
2015-11-30 02:41:22 +0100

    use human readable data to test

--- rb/rb_test2.c
+++ rb/rb_test2.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
-
 #include <pthread.h>
 
 #include "rb.h"
 
-//testing ringbuffer with two threads
+//testing ringbuffer with two uncoordinated, non-waiting threads:
+//a writer thread and a reader thread
+//data pushed through the ringbuffer should keep the same byte values, sequence of bytes
 
 static pthread_t reader_thread={0};
 static pthread_mutex_t reader_thread_lock=PTHREAD_MUTEX_INITIALIZER;
@@ -16,8 +18,8 @@ static pthread_mutex_t writer_thread_lock=PTHREAD_MUTEX_INITIALIZER;
 
 rb_t *rb_=NULL;
 
-#define READER_THREAD 33
-#define WRITER_THREAD 77
+#define READER_THREAD 0
+#define WRITER_THREAD 1
 
 //=============================================================================
 void debug(rb_t *rb, int from_thread)
@@ -60,28 +62,55 @@ static void *reader_thread_func(void *arg)
 {
 	if(rb_==NULL)
 	{
-		fprintf(stderr,"ringbuffer with size 0?\n");
+		fprintf(stderr,"ringbuffer was NULL.\n");
 		exit(1);
 	}
 	debug(rb_,READER_THREAD);
+	//srand(4321);
 
-	srand(4321);
-	while(1==1)//run thread forever
+	size_t buf_size=32; //22 to match write thread content size
+	char buf[buf_size];
+	memset(buf,0,buf_size);
+	size_t read=0;
+	size_t read_total=0;
+
+	while(1==1)
 	{
-		char buf[3];
-		rb_read(rb_,buf,3);//read 3
-		debug(rb_,READER_THREAD);
-
-		int r=rand()/5000;
-		if(r%9==0)//insert drops
+		if(read_total>=buf_size)
 		{
-			fprintf(stderr,"drop\n");
-			rb_drop(rb_);
-			debug(rb_,READER_THREAD);
+			//memset(buf,0,buf_size);
+			read_total=0;
+			fprintf(stdout,"\n");
+			fflush(stdout);
 		}
 
-		fprintf(stderr,"rand %d\n",r);
-		usleep(r); //sleep random
+		read=rb_read(
+			rb_			//read from this ringbuffer
+			,buf+read_total		//read into buf at offset read_total
+			,buf_size-read_total	//request to read remaining bytes to fill buf
+		);
+
+		//buffer to indicate which part was read into buffer
+		char buf_read[read+1]; //+1 for null termination for printing
+		buf_read[read]='\0';
+
+		//copy from main read buffer
+		memcpy(buf_read,buf+read_total,read);
+
+		read_total+=read;
+
+		fprintf(stderr,"read %zu read_total %zu '%s'\n"
+			,read		//bytes read in last rb_read
+			,read_total	//bytes read relative to main read buffer pos 0
+			,buf_read	//contents that were read in last rb_read
+		);
+		fprintf(stdout,"%s",buf_read);
+		fflush(stdout);
+		debug(rb_,READER_THREAD);
+
+		//int r=rand()/50000;
+		//fprintf(stderr,"rand %d\n",r);
+		//usleep(r); //sleep random
 	}
 }
 
@@ -90,27 +119,60 @@ static void *writer_thread_func(void *arg)
 {
 	if(rb_==NULL)
 	{
-		fprintf(stderr,"ringbuffer with size 0?\n");
+		fprintf(stderr,"ringbuffer was NULL.\n");
 		exit(1);
 	}
 	debug(rb_,WRITER_THREAD);
+	//srand(1234);
 
-	srand(1234);
+	//example data buffer
+	char *buf="ringbuffers are cool. ";//\0";
+	size_t content_length=22;
+
+	//in one rb_write operation (reset each cylce)
+	size_t write=0;
+	//relative to full content length (reset after fully written)
+	size_t write_total=0;
+
+	//repeat to write contents of buf to ringbuffer
+	//handle case when rb_write returns less bytes than requested
+	//a reading process should get a stream of identical sentences
 	while(1==1)
 	{
-		char buf[1]={'x'};
-		rb_write(rb_,buf,1);
+		if(write_total>=content_length)
+		{
+			write_total=0;
+//			fprintf(stdout,"\n");
+//			fflush(stdout);
+		}
+
+		write=rb_write(
+			rb_				//write to this ringbuffer
+			,buf+write_total		//from source buffer, starting at offset 'write_total'
+			,content_length-write_total	//request to write all remaining bytes
+		);
+
+		//buffer to indicate which part of source buffer could be written
+		char buf_written[write+1];
+		buf_written[write]='\0';
+
+		//copy from source buffer
+		memcpy(buf_written,buf+write_total,write);
+
+		write_total+=write;
+
+		fprintf(stderr,"write %zu write_total %zu '%s'\n"
+			,write		//bytes written in last rb_write
+			,write_total	//bytes written relative to source buffer pos 0
+			,buf_written	//contents that were written
+		);
+//		fprintf(stdout,"%s",buf_written);
+//		fflush(stdout);
 		debug(rb_,WRITER_THREAD);
 
-		int r=rand()/10000;
-
-		if(r%7==0) //insert write pointer advances
-		{
-			fprintf(stderr,"advance write pointer\n");
-			rb_advance_write_pointer(rb_,7);
-		}
-		fprintf(stderr,"rand %d\n",r);
-		usleep(r); //random sleep
+		//int r=rand()/100000;
+		//fprintf(stderr,"rand %d\n",r);
+		//usleep(r); //random sleep
 	}
 }
 
@@ -133,9 +195,13 @@ static void setup_writer_thread()
 //=============================================================================
 int main(int argc, char *argv[])
 {
+
+	// ./rb_test2 1 >/tmp/out.txt
+	// ./rb_test2 32 2>/dev/null
+
 	if(argc<2)
 	{
-		fprintf(stderr,"need number\n");
+		fprintf(stderr,"need number: ringbuffer size [bytes]\n");
 		exit(1);
 	}
 
@@ -158,7 +224,7 @@ int main(int argc, char *argv[])
 		usleep(100000);
 	}
 
-//	rb_free(rb);
+	//rb_free(rb);
 
 }//end main
 
