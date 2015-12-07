6d26bd37710f0c47d937b92b421a8055bbd1cee1
Thomas Brand <tom@trellis.ch>
2015-12-07 05:29:35 +0100

    add rb_munlock()

--- rb/rb_test0.c
+++ rb/rb_test0.c
@@ -5,12 +5,14 @@ int main()
 	rb_t *rb=rb_new(1);
 	if(rb==NULL) {return 1;}
 	if(!rb_mlock(rb)) {return 1;}
+	if(!rb_munlock(rb)) {return 1;}
 	rb_debug(rb);
 	rb_free(rb);
 
 	rb=rb_new_shared(1);
 	if(rb==NULL) {return 1;}
 	if(!rb_mlock(rb)) {return 1;}
+	if(!rb_munlock(rb)) {return 1;}
 	rb_debug(rb);
 	rb_free(rb);
 
