066c6bf67d43f8b7c6a3aef20248f316b7648723
Thomas Brand <tom@trellis.ch>
2015-12-27 15:00:50 +0100

    negate preprocessor defines, now by default all is enabled, actively DISABLE at compile time with -DRB_xxx. add rb_skip_all() alias to rb_drop(). remove double rb_size(). compile and link variants in Makefile.

--- rb/rb_test0.c
+++ rb/rb_test0.c
@@ -3,16 +3,20 @@
 int main()
 {
 	rb_t *rb=rb_new(1);
-	if(rb==NULL) {return 1;}
-	if(!rb_mlock(rb)) {return 1;}
-	if(!rb_munlock(rb)) {return 1;}
+	if(rb==NULL) {fprintf(stderr,"rb NULL\n");return 1;}
+	rb_debug(rb);
+	if(!rb_mlock(rb)) {fprintf(stderr,"!mlock\n");return 1;}
+	rb_debug(rb);
+	if(!rb_munlock(rb)) {fprintf(stderr,"!munlock\n");return 1;}
 	rb_debug(rb);
 	rb_free(rb);
 
 	rb=rb_new_shared(1);
-	if(rb==NULL) {return 1;}
-	if(!rb_mlock(rb)) {return 1;}
-	if(!rb_munlock(rb)) {return 1;}
+	if(rb==NULL) {fprintf(stderr,"rb shm NULL\n");return 1;}
+	rb_debug(rb);
+	if(!rb_mlock(rb)) {fprintf(stderr,"!mlock\n");return 1;}
+	rb_debug(rb);
+	if(!rb_munlock(rb)) {fprintf(stderr,"!munlock\n");return 1;}
 	rb_debug(rb);
 	rb_free(rb);
 
