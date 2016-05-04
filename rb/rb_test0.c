c5777488d77968325b66f37eac8fc6721304f782
Thomas Brand <tom@trellis.ch>
2016-05-04 22:36:55 +0200

    show sizeof rb_t

--- rb/rb_test0.c
+++ rb/rb_test0.c
@@ -3,6 +3,7 @@
 int main()
 {
 	rb_t *rb=rb_new(1);
+	fprintf(stderr,"sizeof(rb_t): %zu\n",sizeof(rb_t));
 	if(rb==NULL) {fprintf(stderr,"rb NULL\n");return 1;}
 	rb_debug(rb);
 	if(!rb_mlock(rb)) {fprintf(stderr,"!mlock\n");return 1;}
@@ -12,6 +13,7 @@ int main()
 	rb_free(rb);
 
 	rb=rb_new_shared(1);
+	fprintf(stderr,"sizeof(rb_t): %zu\n",sizeof(rb_t));
 	if(rb==NULL) {fprintf(stderr,"rb shm NULL\n");return 1;}
 	rb_debug(rb);
 	if(!rb_mlock(rb)) {fprintf(stderr,"!mlock\n");return 1;}
